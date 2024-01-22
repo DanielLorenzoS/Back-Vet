c09f35611fc933406dbabe8b6dec2bd048478a5e
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2024-01-22 10:53:25 -0600

    Update individual client

--- src/main/java/com/vet/controllers/UserController.java
+++ src/main/java/com/vet/controllers/UserController.java
@@ -39,6 +39,7 @@ public class UserController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size,
             @RequestParam(defaultValue = "name,asc") String[] sort) {
+        System.out.println(lastName);
         return userService.getAllUsersByFilter(name, lastName, email, phone, page, size, sort);
     }
 
